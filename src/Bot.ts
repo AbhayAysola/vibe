ed3db481962975056900305a7c0b782ebd2cdf9b
Abhay Aysola <abhay.ayyasola@gmail.com>
2022-01-03 07:15:19 +0530

    store play-dl auth data in .env

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-import { Client, Intents, Presence, PresenceManager } from "discord.js";
+import { Client, Intents } from "discord.js";
+import play from "play-dl";
 
 import logger from "./utils/logger";
 import ready from "./listeners/ready";
@@ -18,5 +19,17 @@ ready(client);
 interactionCreate(client);
 
 client.login(token);
+
+play.setToken({
+  youtube: {
+    cookie: process.env.youtube_cookie || "",
+  },
+  spotify: {
+    client_id: process.env.spotify_client_id || "",
+    client_secret: process.env.spotify_client_secret || "",
+    refresh_token: process.env.spotify_refresh_token || "",
+    market: "IN",
+  },
+});
 
 // https://discord.com/api/oauth2/authorize?client_id=926462122165166180&permissions=2150631424&scope=bot%20applications.commands
