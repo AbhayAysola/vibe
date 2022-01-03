f6e02709614c2813ecf44825ac197c74b45037f0
Abhay Aysola <abhay.ayyasola@gmail.com>
2022-01-03 13:51:47 +0530

    add Pause Stop and Play

--- src/Commands.ts
+++ src/Commands.ts
@@ -2,5 +2,6 @@ import { Command } from "./Command";
 import { Queue } from "./commands/Queue";
 import { Play } from "./commands/Play";
 import { Stop } from "./commands/Stop";
+import { Pause } from "./commands/Pause";
 
-export const Commands: Command[] = [Queue, Play, Stop];
+export const Commands: Command[] = [Queue, Play, Stop, Pause];
--- src/commands/Stop.ts
+++ src/commands/Stop.ts
@@ -1,13 +1,9 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { Client, CommandInteraction, MessageEmbed } from "discord.js";
+import logger from "../utils/logger";
 
 import { Command } from "../Command";
-import {
-  disconnect,
-  errorEmbed,
-  checkVoiceChannel,
-  defaultColor,
-} from "../Common";
+import { queue, errorEmbed, checkVoiceChannel, defaultColor } from "../Common";
 
 export const Stop: Command = {
   data: new SlashCommandBuilder()
@@ -19,7 +15,26 @@ export const Stop: Command = {
       return;
     }
     if (await checkVoiceChannel(interaction)) {
-      disconnect(interaction);
+      const serverQueue = queue.get(interaction.guildId || "");
+      if (!serverQueue) {
+        await interaction.followUp({
+          embeds: [errorEmbed("I am not connected to a voice channel!")],
+          ephemeral: true,
+        });
+        return;
+      }
+      serverQueue?.connection?.destroy();
+      serverQueue?.audioPlayer?.removeAllListeners();
+      logger.info("Destroying connection");
+      queue.delete(interaction.guildId || "");
+      interaction.followUp({
+        embeds: [
+          new MessageEmbed()
+            .setTitle("Disconnected")
+            .setDescription("Bot has cleared the queue.")
+            .setColor(defaultColor),
+        ],
+      });
     }
   },
 };
